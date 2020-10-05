02bf7f10f563aaf29a55fbf594ebcba1fd535c7f
Kuanting Pan <pankuanting@gmail.com>
2020-10-05 01:48:44 -0400

    test create read me function

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,40 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { promisify } = require("util");
+const writeAsycn = util.promisify(fs.writeFile)
 // array of questions for user
 const questions = [
+    {
+        message: "What is your title",
+        name: "title",
+    },
 
+    {
+        message: "What is your name",
+        name: "name",
+    },
+
+    {
+        message: "What is your age",
+        name: "age",
+    },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+async function writeToFile(fileName, data) {
+    await writeAsync(fileName, data);
+    if (err){
+        throw err;
+    }
 }
 
 // function to initialize program
-function init() {
-
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    const htmlString = generateMarkdown(answers)
+    await writeAsycn(answers.name + ".md", htmlString)
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+### Welcome to ${data.title}'s page
+### Use
+  - like so...
+  - my age: ${data.age}
 
-`;
+`
 }
 
 module.exports = generateMarkdown;
